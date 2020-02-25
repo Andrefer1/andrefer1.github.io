8d03934761d8061261165ac778c4f28f3a526e31
André Fernandes <andrefer483@gmail.com>
2020-02-25 03:41:23 -0300

    Adding the header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,9 +1,26 @@
 import React from 'react'
 
 import './index.css'
+import me from '../../assets/header/Me.png'
 
 export default function Header() {
     return (
-        <h1>Header</h1>
+        <header>
+            <section className="section-header">
+                <div className='pht'>
+                    <img src={me} alt='My photo' className='photo size-photo img-fluid' />
+                </div>
+                <div className="my-datas">
+                    <div className='name'>André F. Bispo</div>
+                    <div className='graduation'>Software Engineer</div>
+                </div>
+
+                <div className='header-buttons'>
+                    <a href='#' className="portfolio">Portfolio</a>
+                    <a href='#' className="what-i-do">What I do?</a>
+                </div>
+
+            </section>
+        </header>
     )
 }
