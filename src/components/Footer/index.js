a33aa61367818724676cc1b6d8de15fa74fa6780
André Fernandes <andrefer483@gmail.com>
2020-02-25 04:42:35 -0300

    Testing build

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -13,16 +13,29 @@ export default function Footer() {
             <section className="section-footer">
                 <div className="follow-me">
                     Contact me
-           </div>
-                <div className="social-media">
-                    <a href='https://www.instagram.com/dev.andref/' target='_blank'><img src={instagram} alt="Instagram" className="instagram" /></a>
-                    <a href='https://www.linkedin.com/in/andrefbispo/' target='_blank'><img src={linkedin} alt="Linkedin" className="linkedin" /></a>
-                    <a href='https://github.com/Andrefer1' target='_blank'><img src={github} alt="GitHub" className="github" /></a>
-                    <a href='' target='_blank'><img src={email} alt="Email" className="email" /></a>
                 </div>
+
+                <div className="social-media">
+                    <a href='https://www.instagram.com/dev.andref/' target='_blank'>
+                        <img src={instagram} alt="Instagram" className="instagram" />
+                    </a>
+                    
+                    <a href='https://www.linkedin.com/in/andrefbispo/' target='_blank'>
+                        <img src={linkedin} alt="Linkedin" className="linkedin" />
+                    </a>
+
+                    <a href='https://github.com/Andrefer1' target='_blank'>
+                        <img src={github} alt="GitHub" className="github" />
+                    </a>
+
+                    <a href='https://mail.google.com/mail/u/0/#inbox?compose=GTvVlcSMTRzfXpnZnVxqQZqNFltZsnvbDpQMLDGVdqFMqSgPpSxqKPLfTJfmcXTJQrHBqjzrjghhM' target='_blank'>
+                        <img src={email} alt="Email" className="email" />
+                    </a>
+                </div>
+
                 <div className="message">
                     Made by ANDRÉ®
-           </div>
+                </div>
             </section>
         </footer>
     )
--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -5,7 +5,20 @@ import './index.css'
 class Main extends Component {
     render() {
         return (
-            <h1>Main</h1>
+            <main>
+                <section className="section-main">
+                    <div className="img-1"></div>
+                    <div className="img-2"></div>
+                    <div className="img-3"></div>
+                    <div className="img-4"></div>
+                </section>
+                {/* I was born on February 3, 1998, I live in Palmas, TO, Brazil, I am male, not married, 
+                currently work as a trainee at the Regional Electoral Tribunal, TRE in Portuguese and study 
+                at Lutheran University Center of Palmas (CEULP). Aos 15 anos entrei no Instituto Federal do 
+                Tocantins (IFTO), onde concluí o Ensino Médio regular e tive a oportunidade de fazer um curso 
+                de Mecatrônica. Aos 19 anos, após muitas dúvidas sobre qual caminho seguiria, decidi que faria 
+                TI por ser um sonho de infância. */}
+            </main>
         )
     }
 }
