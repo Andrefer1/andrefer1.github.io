09fa9c8336bc8c8ef071fcf89497eead9fda05fb
André Fernandes <andrefer483@gmail.com>
2020-02-25 03:44:17 -0300

    Adding the footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -2,8 +2,28 @@ import React from 'react'
 
 import './index.css'
 
+import instagram from '../../assets/footer/instagram.svg'
+import linkedin from '../../assets/footer/linkedin.svg'
+import github from '../../assets/footer/github.svg'
+import email from '../../assets/footer/email.svg'
+
 export default function Footer() {
-    return(
-        <h1>Footer</h1>
+    return (
+        <footer>
+            <section className="section-footer">
+                <div className="follow-me">
+                    Contact me
+           </div>
+                <div className="social-media">
+                    <a href='https://www.instagram.com/dev.andref/' target='_blank'><img src={instagram} alt="Instagram" className="instagram" /></a>
+                    <a href='https://www.linkedin.com/in/andrefbispo/' target='_blank'><img src={linkedin} alt="Linkedin" className="linkedin" /></a>
+                    <a href='https://github.com/Andrefer1' target='_blank'><img src={github} alt="GitHub" className="github" /></a>
+                    <a href='' target='_blank'><img src={email} alt="Email" className="email" /></a>
+                </div>
+                <div className="message">
+                    Made by ANDRÉ®
+           </div>
+            </section>
+        </footer>
     )
 }
--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,6 +1,7 @@
 import React from 'react'
 
 import './index.css'
+
 import me from '../../assets/header/Me.png'
 
 export default function Header() {
